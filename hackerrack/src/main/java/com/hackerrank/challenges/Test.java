f80f3af5294363cb56bf9068834a582871ed57c7
nanditha16 <nanditha.murthy@gmail.com>
2019-09-07 14:39:58 -0500

    Practice Challenge

---
+++ hackerrack/src/main/java/com/hackerrank/challenges/Test.java
@@ -0,0 +1,29 @@
+package com.hackerrank.challenges;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> packagesSpace =  new ArrayList<Integer>();
+		packagesSpace.add(1);
+		packagesSpace.add(10);
+		packagesSpace.add(25);
+		packagesSpace.add(35);
+		packagesSpace.add(60);
+		
+		IDsOfPackages(90, packagesSpace);
+	}
+	
+	// METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+	public static ArrayList<Integer> IDsOfPackages(int truckSpace, 
+	                                 ArrayList<Integer> packagesSpace)
+	{
+		System.out.println("Input:" +  truckSpace + packagesSpace);
+		
+		
+		return packagesSpace;
+	}
+
+}
